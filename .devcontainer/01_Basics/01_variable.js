44a3c12ed588ad7e451af00324c0f85d768426d0
ada90333 <98938474+ada90333@users.noreply.github.com>
2024-09-20 10:12:26 +0000

    datatypes

--- .devcontainer/01_Basics/01_variable.js
+++ .devcontainer/01_Basics/01_variable.js
@@ -2,4 +2,6 @@ accountID = 'adarsh90333'
 let accountName = 'Adarsh'
 const accountEmail = 'adarsh@gmail.com'
 
+console.log(typeof accountEmail)
 console.log(accountEmail, accountName, accountID)
+console.table([accountEmail, accountName, accountID])
---
+++ .devcontainer/01_Basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"
+
+//alert(3+3)  // we are using nodejs, not browser
+
+let name = 'adarsh'
+let number = 34
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigInt
+//string
+//boolean  => true/false
+// null --> standalone value (type)
+// undefined ==> only declaration not assigned any value
+
+// object
+
+console.log(typeof undefined) //undefined
+
+console.log(typeof null) // object
