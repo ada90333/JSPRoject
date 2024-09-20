789a7af78af9a608fa3dd98cee4213c25d89da39
ada90333 <98938474+ada90333@users.noreply.github.com>
2024-09-20 10:25:26 +0000

    conversion of datatypes

---
+++ .devcontainer/01_Basics/03_conversoinOperatoin.js
@@ -0,0 +1,30 @@
+let score = null
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1; false => 0
+
+let isloggedIn = "Hitesh"
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn)
+
+
+// 1=> true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let number = 34
+
+let stringNumber = String(number)
+
+console.log(stringNumber)
+console.log(typeof(stringNumber))
