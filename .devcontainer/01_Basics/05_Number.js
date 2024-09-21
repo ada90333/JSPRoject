0133b1fcfeda4ec04b2658aef8245c3e3ad38e25
ada90333 <98938474+ada90333@users.noreply.github.com>
2024-09-21 14:32:46 +0000

    Number and Math

---
+++ .devcontainer/01_Basics/05_Number.js
@@ -0,0 +1,18 @@
+const score = new Number(400)
+
+// console.log(score)
+
+// console.log(score.toString().length)
+// console.log(score.toFixed(2))
+
+
+
+const hundreds =1000000
+
+//console.log(hundreds.toLocaleString('en-IND'))
+
+
+// Maths   --> BY default lib inside JS
+
+
+console.log(Math)
