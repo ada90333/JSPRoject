58d976cc9e2a4a7446a27d91811fc3d004f4eed4
ada90333 <98938474+ada90333@users.noreply.github.com>
2024-09-21 15:00:25 +0000

    array object

---
+++ .devcontainer/02_Basics/02_array.js
@@ -0,0 +1,28 @@
+const marvel = [ 'thor', 'Ironman', 'spiderMan']
+const dc_hero = ['superman', 'flash', ' batman']
+
+
+//marvel.push(dc_hero)
+
+// console.log(marvel)
+// console.group(dc_hero)
+//marvel.concat(dc_hero)
+
+// const allHero = marvel.concat(dc_hero)
+
+// console.log(allHero)
+
+// const allHero = [...marvel, ...dc_hero]
+
+//console.log(allHero)
+
+// const anoArray= [1,2,[3,4],5, [6,7,[8,9]]]
+
+// const realanoArray = anoArray.flat(Infinity)
+// console.log(anoArray)
+// console.log(realanoArray)
+
+
+console.log(Array.isArray('Adarsh'))
+console.log(Array.from('Adarsh'))
+console.log(Array.from({name:'Adarsh'})) // Interesting case for interviews
